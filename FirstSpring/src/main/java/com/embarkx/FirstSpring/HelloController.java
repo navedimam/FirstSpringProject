b6ab1a17bb369a1e064a46979907ef4ac5880340
Naved <naved.imam786@gmail.com>
2025-12-12 23:13:19 +0530

    committed

---
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -0,0 +1,29 @@
+package com.embarkx.FirstSpring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello/{name}")
+	public HelloResponse helloParam(@PathVariable String name) {
+		return new HelloResponse("Hello, "+name+"!");
+	}
+	
+	@GetMapping("/hello")
+	public HelloResponse hello() {
+		return new HelloResponse("Hello World!");
+	}
+	
+	@PostMapping("/hello")
+	public HelloResponse helloPost(@RequestBody String name) {
+		return new HelloResponse("Hello" + name+"!");
+	}
+
+
+
+}
