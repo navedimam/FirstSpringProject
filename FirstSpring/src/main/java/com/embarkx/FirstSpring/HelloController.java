ddd193e223718470c65ee68bd616d43e9e3ac162
Naved <Naved@DESKTOP-6B8PN39>
2025-12-14 12:49:48 +0530

    new API

--- FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
 	
 	@GetMapping("/newApi/hello")
 	public HelloResponse hello1() {
-		return new HelloResponse("Hello World! New API");
+		return new HelloResponse("Hello World! New API by Tabrez");
 	}
 	
 	@PostMapping("/hello")
